d586a57a12a0052e67746ebb4bce175aba956e3a
arno9982 <arnotsague04@gmail.com>
2026-06-02 17:53:58 +0100

    update api url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -3,7 +3,7 @@
 import { useEmpruntTiers } from "../hooks/useEmpruntTiers";
 import { createLoan } from "../services/loan.service";
 
-export const API_BASE_URL = "https://mutuelle-backend-m1-gi-2025-2026.onrender.com";
+export const API_BASE_URL = "https://mutuelle-backend-m1-gi-2025-2026.onrender.com/api";
 
 export const API_ENDPOINTS = {
   // Auth
